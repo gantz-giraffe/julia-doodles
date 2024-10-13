379bc4d1e8471c40182067435bd534e60d9940d8
gantz-giraffe <33939972+gantz-giraffe@users.noreply.github.com>
2024-10-12 22:59:23 -0500

    Update curl.jl

--- doodles/curl/curl.jl
+++ doodles/curl/curl.jl
@@ -10,9 +10,9 @@ img = fill(RGB(1.0,1.0,1.0),(500,500))
 
 for j in 1:500
     for k in 1:500
-        n = noise(j/100,k/100)
-        c = curl(noise,Point(j/100,k/100))
-        img[j,k] = n
+        #n = noise(j/10,k/10)
+        c = curl(funnyfunc,Point(j/1,k/1))
+        img[j,k] = RGB(c.x,c.y,0)
     end
 end
 img
@@ -27,14 +27,21 @@ function flowline(func, pos::Point,iterations;step = 5)
     pts
 end
 
+funnyfunc(x,y) = ((x + sin(x)*cos(y)*2)*0.01+(y+cos(x*y/2))*0.01)
+
 begin
-    Drawing(500,500)
+    w = 1500; h = 1500
+    Drawing(w,h)
     background("white")
+    setcolor(0,0,0,0.6666)
     setline(1)
-        for i in 1:2500
-            p = randompoint(Point(0,0),Point(500,500))
-            poly(flowline((a,b)->noise(a*0.01,b*0.01),p,rand(2000:3000),step = 100),action = :stroke)
+        for i in 1:2000
+            p = randompoint(Point(0,0),Point(w,h))
+            poly(flowline((a,b)->funnyfunc(a/15+500,b/1.9+251),p,rand(2000:3000),step = 250),action = :stroke)
         end
+        pic = image_as_matrix()
         finish()
-        preview()
+        #preview()
+        save(string("E:/code/Julia/julia-doodles-out/curl_out/curl_",time_ns(),".png"),map(clamp01nan,pic))
+        pic
 end
